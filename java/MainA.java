305b0ee7093cc7a592cdc6d17442b2d0c4b0a7fc
Adam359 <adam359@hawaii.edu>
2017-02-25 15:34:23 -1000

    init

---
+++ java/MainA.java
@@ -0,0 +1,7 @@
+package test;
+
+public class MainA {
+  public static void main(String[] args) {
+    MainB.main(args);
+  }
+}
---
+++ java/MainB.java
@@ -0,0 +1,8 @@
+package test;
+
+public class MainB {
+
+  public static void main(String[] args) {
+    System.out.println("[MainB]Hello world!");
+  }
+}
